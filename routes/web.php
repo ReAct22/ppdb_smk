e533e87699db11c62daa268ab8e8866c874ceaeb
Andreanahmad <andreanahmadf@gmail.com>
2021-06-21 03:54:53 +0700

    Pembaruan page admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,8 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::prefix('admin')
+->group(function(){
+    Route::get('/', [AdminController::class, 'index'])->name('dashboard');
+});
+
